13cfe50058bd08b9cf95dd1f89772f02ff127ca4
Jongita <jurgitajonaityte@gmail.com>
2024-01-31 08:50:58 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// Single-line comment
+// Single-line comment
+// Single-line comment
+
+/*
+
+Busimos temos:
+- duomenu tipai
+    - skaiciai
+    - tekstas
+    - logines reiksmes
+    - sarasas 
+    - objektas
+- salygos (if)
+- ciklai (for)
+- funkcijos
+- ivykiai (event listeners)
+
+*/
+
+console.log(123);
+console.log(123123);
+console.log('1+1');
+
+
