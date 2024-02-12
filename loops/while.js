f37c3abd108b55f48bab1c4cf4eb7f51fcc861a5
Jongita <jurgitajonaityte@gmail.com>
2024-02-12 12:38:19 +0200

    while

---
+++ loops/while.js
@@ -0,0 +1,23 @@
+
+for (let i = 10; i <= 20; i++) {
+
+
+}
+
+let i = 10;
+for (; i <= 15;) {
+    console.log(i);
+    i++;
+}
+// while - daryk tol kol gali
+console.log('-------------');
+
+let k = 10;
+while (k <= 15) {
+    console.log(k);
+    k++;
+}
+
+// for ir while - vienodi
+// while naudojamas retai, kai nezinai kiek suksis ciklas, bet zinai kada turi sustoti
+// while patogus skaityti, kitu pliusu neturi
